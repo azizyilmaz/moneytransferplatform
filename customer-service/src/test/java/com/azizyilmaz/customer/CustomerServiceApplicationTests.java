05ca52b44604c5a5604d55e41db06ef256e8491b
azizyilmaz <azizxyilmaz@outlook.com>
2025-10-04 06:33:29 +0300

    Money Transfer Platform Project

---
+++ customer-service/src/test/java/com/azizyilmaz/customer/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azizyilmaz.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
